cf02b582267d9681751410b634ed33845966e02a
Duben55 <dubenchan@gmail.com>
2024-02-11 17:03:44 +0400

    unnecessary comments and lines have been removed

--- task-1.php
+++ task-1.php
@@ -1,25 +1,6 @@
 <?php
-
 //На вход подается строка из чисел, разделенных пробелами.
 //Переместите все нули в конец строки. Порядок остальных чисел должен сохраниться.
-//Input
-//7 0 39 0 282 2 4 0 45
-//Output
-//7 39 282 2 4 45 0 0 0
-//
-//$inputArray = [7, 0, 39, 0, 282, 2, 4, 0, 45];
-//$zeroCount = 0;
-//
-//foreach ($inputArray as $value) {
-//    if ($value != 0) {
-//        echo $value . ' ';
-//    }
-//    else {
-//        $zeroCount++;
-//    }
-//}
-//
-//echo str_repeat('0 ', $zeroCount);
 
 $inputArray = [7, 0, 39, 0, 282, 2, 4, 0, 45];
 $zeros = [];
@@ -28,8 +9,7 @@ $arrayWithoutZero = [];
 foreach ($inputArray as $value) {
     if ($value != 0) {
         $arrayWithoutZero[] = $value;
-    }
-    else {
+    } else {
         $zeros[] = $value;
     }
 }
