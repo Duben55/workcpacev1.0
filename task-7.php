39221a71dfd477b0b7bfe939924a40fb20f97828
Duben55 <dubenchan@gmail.com>
2024-02-11 17:39:13 +0400

    added a description of the work done

--- task-7.php
+++ task-7.php
@@ -1,35 +1,46 @@
 <?php
+//создали класс котиков с тремя свойствами и 2 объекта класса (рыжий и белый),
+//конструктор, что бы при создании котика указвать его свойства,
+//применил $this, изучил геттеры и сеттеры, модификаторы доступа.
+//кот рассказывает о себе методом sayHello()
+
 final class Cat
 {
     private string $name;
     private string $color;
     private float $weight;
+
     public function __construct(string $name, string $color, float $weight)
     {
         $this->name = $name;
         $this->color = $color;
         $this->weight = $weight;
     }
+
     public function sayHello(): void
     {
         echo 'Привет! Меня зовут ' . $this->name . '.' . PHP_EOL;
         echo 'Мой цвет - ' . $this->color . '.' . PHP_EOL;
         echo 'Мой вес равен ' . $this->weight . ' килограмм.' . PHP_EOL;
     }
-    public function setCatData(string $name, string $color, float $weight) : void
+
+    public function setCatData(string $name, string $color, float $weight): void
     {
         $this->name = $name;                    //сеттер
         $this->color = $color;
         $this->weight = $weight;
     }
+
     public function getName(): string                    //геттер
     {
         return $this->name;
     }
+
     public function getColor(): string                    //геттер
     {
         return $this->color;
     }
+
     public function getWeight(): string                    //геттер
     {
         return $this->weight;
@@ -39,8 +50,5 @@ final class Cat
 $cat1 = new Cat('Снежок', 'Белый', 3.5);
 $cat1->sayHello();
 
-$cat1->setCatData('Черт', 'Рыжий', 4.6);
-$cat1->sayHello();
-//echo $cat1->getName() . PHP_EOL;
-//echo $cat1->getColor() . PHP_EOL;
-//echo $cat1->getWeight() . PHP_EOL;
+$cat2 = new Cat('Черт', 'Рыжий', 4.6);
+$cat2->sayHello();
