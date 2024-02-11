c3502df59cc97107e2c30934bb490927210f5dcd
Duben55 <dubenchan@gmail.com>
2024-02-11 17:22:43 +0400

    removed if, replaced one for with foreach

--- task-2.php
+++ task-2.php
@@ -2,20 +2,14 @@
 //На вход подается строка целых чисел, разделенных пробелами.
 //Найдите максимальную разницу между двумя элементами строки при условии,
 //что меньшее число должно находиться справа от большего
-//Input
-//1 6 4 3
-//Output
-//3
 
-$inputArray = [1, 6, 4, 3, 10, 5, 111];
+$inputArray = [-10, 16, 4, 3, 10, 5, -3];
 $result = null;
-
-for ($i = 0; $i <= count($inputArray) - 2; $i++) {
-    for ($j = $i + 1; $j <= count($inputArray) - 1; $j++) {
-        if ($i < $j) {
-            if ($inputArray[$i] - $inputArray[$j] > $result) {
-                $result = $inputArray[$i] - $inputArray[$j];
-            }
+$countArray = count($inputArray);
+foreach ($inputArray as $key => $value){
+    for ($j = $key + 1; $j <= $countArray - 1; $j++) {
+        if ($value - $inputArray[$j] > $result) {
+            $result = $value - $inputArray[$j];
         }
     }
 }
